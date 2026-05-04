fb80e0fa63ead3d730a86dd8f9548870d07d1f35
Nath <nathalycis20@gmail.com>
2026-05-04 00:44:51 -0500

    comentando Clase Hampon

--- src/main/java/co/ciencias/hampones/model/Hampon.java
+++ src/main/java/co/ciencias/hampones/model/Hampon.java
@@ -1,20 +1,40 @@
 package co.ciencias.hampones.model;
 
+/**
+ * Representa la entidad de un Hampon dentro del sistema.
+ * 
+ * @author 
+ */
+
 public class Hampon {
     private int edad;
     private int dinero;
     private String nombre;  
-
+    
+     /**
+     * Constructor por defecto.
+     * Inicializa un objeto Hampon con valores base (0 para números y cadena vacía para el nombre).
+     */
     public Hampon(){
         edad = 0;
         dinero = 0;
         nombre = "";
     }
+    
+    /**
+     * Constructor con parámetros para inicializar todos los atributos de la clase.
+     * 
+     * @param edad El valor entero para la edad inicial.
+     * @param dinero El capital inicial asignado al hampón.
+     * @param nombre El nombre o alias identificativo.
+     */
     public Hampon(int edad, int dinero, String nombre){
         this.edad = edad;
         this.dinero = dinero;
         this.nombre = nombre;
     }
+    
+    // GETTERS
     public int getEdad() {
         return edad;
     }
@@ -24,6 +44,8 @@ public class Hampon {
     public String getNombre() {
         return nombre;
     }
+    
+    // SETTERS
     public void setEdad(int edad) {
         this.edad = edad;
     }
@@ -33,4 +55,16 @@ public class Hampon {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    //TO STRING
+    /**
+     * Genera una representación en texto del estado actual del hampon.
+     * 
+     * @return Una cadena formateada que incluye la edad, el dinero y el nombre.
+     */
+    @Override
+    public String toString() {
+        return "Hampon{" + "edad=" + edad + ", dinero=" + dinero + ", nombre=" + nombre + '}';
+    }
+    
 }
